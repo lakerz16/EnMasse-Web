c53edcd18928b06f7525e4513f5d2971ea3b58a1
Matt Bialas <matt@new-host-3.home>
2013-10-03 18:30:58 -0400

    initial rails project

---
+++ EnMasse/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnMasse::Application.config.secret_token = '7add661eecfe2b4aa3157a0faaa3e94f89272e1ccc735746751f425e041c52c032a37e588befce0422fc9294956e4c42e6ad02cb84ca6b2e1a8dc7d47a21d79e'
